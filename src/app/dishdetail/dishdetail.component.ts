ee0802590cbc8533ab383670601f174576bac088
Pranil <pranilbhalerao99@gmail.com>
2020-06-01 23:10:21 +0530

    Animations part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,12 +3,11 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { switchMap } from 'rxjs/operators';
-import { trigger, state, style, animate, transition } from '@angular/animations'
 
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { baseURL } from '../shared/baseurl';
-
+import { visibility } from '../animations/app.animations'
 
 
 @Component({
@@ -16,17 +15,7 @@ import { baseURL } from '../shared/baseurl';
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
   animations: [
-    trigger('visibility',[
-      state('shown', style({
-        transform: 'scale(1.0)',
-        opacity: 1
-      })),
-      state('hidden', style({
-        transform: 'scale(0.5)',
-        opacity: 0
-      })),
-      transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    visibility()
   ]
 })
 export class DishdetailComponent implements OnInit {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,11 +7,20 @@ import { LeaderService } from '../services/leader.service';
 import { Leader } from '../shared/leader';
 import { baseURL } from '../shared/baseurl'
 import { HttpErrorResponse } from '@angular/common/http';
+import { flyInOut, expand } from '../animations/app.animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+    flyInOut(),
+    expand()
+  ]
 })
 export class HomeComponent implements OnInit {
   dish: Dish;
